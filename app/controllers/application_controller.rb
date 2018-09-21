03772a5067fb920b70d645760014e5d5d8958bcc
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-09-21 13:44:53 +0000

    added get and post signup routes to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,15 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
   
+  get '/signup' do
+    erb :signup
+  end
+  
+  post '/signup' do
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+      @user.save
+      session[:user_id] = @user.id
+  end
+  
 end
