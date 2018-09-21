fda6a91248abb44214af0d80e9e96e637b5bc6a4
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-09-21 13:53:33 +0000

    added helpers, get birthdays routes to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,4 +33,23 @@ class ApplicationController < Sinatra::Base
     session[:user_id] = @user.id
   end
   
+  get '/birthdays' do
+    if logged_in?
+      @user == current_user
+      @birthdays == @user.birthdays
+      erb :'birthdays/index'
+    end
+  end
+  
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+  
+  
 end
